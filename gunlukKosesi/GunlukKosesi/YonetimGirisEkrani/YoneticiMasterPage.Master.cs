b1785928efe83ad7684e362228d5f852b66758af
lincarsia <kocaoglufurkan818@gmail.com>
2024-09-22 17:46:18 +0300

    Yönetici masterpage ve css yapıldı

--- gunlukKosesi/GunlukKosesi/YonetimGirisEkrani/YoneticiMasterPage.Master.cs
+++ gunlukKosesi/GunlukKosesi/YonetimGirisEkrani/YoneticiMasterPage.Master.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using VeriErisimKatmani;
 
 namespace GunlukKosesi.YonetimGirisEkrani
 {
@@ -11,6 +12,22 @@ namespace GunlukKosesi.YonetimGirisEkrani
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["GirisYapanYonetici"] != null)
+            {
+                Yonetici y = (Yonetici)Session["GirisYapanYonetici"];
+                lbl_kullanici.Text = y.KullaniciAdi + " (" + y.YoneticiTur + ")";
+            }
+            else
+            {
+                Response.Redirect("Giris.aspx");
+            }
+
+        }
+
+        protected void lbtn_cikis_Click(object sender, EventArgs e)
+        {
+            Session["GirisYapanYonetici"] = null;
+            Response.Redirect("GirisEkrani.aspx");
 
         }
     }
